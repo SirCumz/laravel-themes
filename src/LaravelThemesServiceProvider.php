ba5ec6a6f839f5895319a099e27b326bab5349de
PPPokeHerFace <jefferso_83@hotmail.com>
2017-10-05 15:48:21 +0200

    removed helper include

--- src/LaravelThemesServiceProvider.php
+++ src/LaravelThemesServiceProvider.php
@@ -37,8 +37,6 @@ class LaravelThemesServiceProvider extends ModuleServiceProvider
      */
     public function register()
     {
-        require_once(__DIR__ . '/helpers.php');
-
         $this->publishes([
             __DIR__.'/config/theme.php' => config_path('theme.php'),
         ]);
